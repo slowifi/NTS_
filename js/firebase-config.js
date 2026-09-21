1c449029b49b5602d4fe55c96663f5a3049eedc2
slowifi <okc05181@gmail.com>
2026-09-21 15:26:23 +0900

    Replace REPLACE_ME

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.13.2/firebas
 
 // Firebase 콘솔 > 프로젝트 설정 > 일반 > 내 앱(웹)에서 발급받은 값으로 교체하세요.
 const firebaseConfig = {
-  apiKey: 'REPLACE_ME',
-  authDomain: 'REPLACE_ME.firebaseapp.com',
-  projectId: 'REPLACE_ME',
-  storageBucket: 'REPLACE_ME.appspot.com',
-  messagingSenderId: 'REPLACE_ME',
-  appId: 'REPLACE_ME',
+  apiKey: "AIzaSyC6gTJYKj6oANYa7jr9lle-kdw76PDK5bE",
+  authDomain: "nts2026.firebaseapp.com",
+  projectId: "nts2026",
+  storageBucket: "nts2026.firebasestorage.app",
+  messagingSenderId: "618068905368",
+  appId: "1:618068905368:web:c0234229b2722dbb9fefed"
 };
 
 const app = initializeApp(firebaseConfig);
